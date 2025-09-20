93a9319f93581def8e3e312e18d7aa9710a5111e
App Generator <51070104+app-generator@users.noreply.github.com>
2025-09-20 16:55:07 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Baserequest(models.Model):
+
+    #__Baserequest_FIELDS__
+    requestid = models.TextField(max_length=255, null=True, blank=True)
+    requestdate = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    requesttype = models.TextField(max_length=255, null=True, blank=True)
+
+    #__Baserequest_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Baserequest")
+        verbose_name_plural = _("Baserequest")
+
+
+
+#__MODELS__END
